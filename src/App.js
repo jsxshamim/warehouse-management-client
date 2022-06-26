edce2fa46a1f16734586cfcb7de9e82b2037e44f
Shamim Reza <wpshamim@gmail.com>
2022-06-26 12:07:48 +0600

    add not found page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import "./App.css";
 import Home from "./Components/Home/Home/Home";
 import Header from "./Components/Shared/Header/Header";
 import { Routes, Route } from "react-router-dom";
+import Footer from "./Components/Shared/Footer/Footer";
+import Inventories from "./Components/ManageInventories/Inventories/Inventories";
+import NotFound from "./Components/Shared/NotFound/NotFound";
+import Login from "./Components/Authentication/Login/Login";
+import InventoryDetails from "./Components/ManageInventories/InventoryDetails/InventoryDetails";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
@@ -9,7 +16,13 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/manage-inventories" element={<Inventories />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/inventory/:inventoryID" element={<InventoryDetails />} />
+
+                <Route path="*" element={<NotFound />} />
             </Routes>
+            <ToastContainer />
         </>
     );
 }
---
+++ src/Components/Shared/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <div className="container mx-auto pt-24">
+            <h1>Page Not Found | 404</h1>
+        </div>
+    );
+};
+
+export default NotFound;
