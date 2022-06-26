1f334d1d4aaac6fc8aecdfba782cc5938d8579ec
Shamim Reza <wpshamim@gmail.com>
2022-06-26 12:07:22 +0600

    navbar added for each pages

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import Logo from "../../../Images/EazyStock_Logo.webp";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faFileInvoice, faSignOut, faSignIn } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = React.useState(false);
+    const [show, setShow] = useState(true);
+
+    console.log();
     return (
         <section>
-            <nav className="relative flex flex-wrap items-center justify-between py-3 bg-white mb-3">
+            <nav className={`relative flex md:fixed md:w-full flex-wrap items-center justify-between z-40 py-3 bg-white shadow-lg`}>
                 <div className="container mx-auto flex flex-wrap items-center justify-between">
                     <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
                         <a className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-gray-800" href="#pablo">
@@ -20,24 +26,63 @@ const Navbar = () => {
                         </button>
                     </div>
                     <div className={"lg:flex flex-grow items-center" + (navbarOpen ? " flex" : " hidden")} id="example-navbar-danger">
-                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                        <ul className="flex flex-col gap-5 items-center lg:flex-row list-none lg:ml-auto">
                             <li className="nav-item">
-                                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-800 hover:opacity-75" href="#pablo">
-                                    <i className="fab fa-facebook-square text-lg leading-lg text-gray-800 opacity-75"></i>
-                                    <span className="ml-2">Share</span>
-                                </a>
+                                <NavLink className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 hover:opacity-75" to="/">
+                                    Home
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-800 hover:opacity-75" href="#pablo">
-                                    <i className="fab fa-twitter text-lg leading-lg text-gray-800 opacity-75"></i>
-                                    <span className="ml-2">Tweet</span>
-                                </a>
+                                <NavLink className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-gray-800 hover:opacity-75" to="/manage-inventories">
+                                    Manage Inventory
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-gray-800 hover:opacity-75" href="#pablo">
-                                    <i className="fab fa-pinterest text-lg leading-lg text-gray-800 opacity-75"></i>
-                                    <span className="ml-2">Pin</span>
-                                </a>
+                                <NavLink className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-title hover:opacity-75" to="/blog">
+                                    Blog
+                                </NavLink>
+                            </li>
+                            <li onClick={() => setShow(!show)} className="hidden nav-item cursor-pointer relative">
+                                <div className="flex items-center gap-3">
+                                    <img className="w-14 h-14 rounded-full border p-1" src="https://pixelwibes.com/template/ebazar/laravel/public/backend/dist/assets/images/profile_av.svg" alt="" />
+                                </div>
+                                {show && (
+                                    <ul className="absolute top-[70px] right-0">
+                                        <div className="rounded shadow bg-white " data-bs-popper="none">
+                                            <div className="w-[240px] p-5">
+                                                <div className="card-body pb-0">
+                                                    <div className="flex py-1 items-center gap-2">
+                                                        <img className="rounded-full w-10 h-10" src="https://pixelwibes.com/template/ebazar/laravel/public/backend/dist/assets/images/profile_av.svg" alt="profile" />
+                                                        <div className="flex-fill ms-3 ">
+                                                            <h4 className="font-bold">John Quinn</h4>
+                                                            <small className="">Johnquinn@gmail.com</small>
+                                                        </div>
+                                                    </div>
+
+                                                    <div>
+                                                        <div className="border-b-2 my-3" />
+                                                    </div>
+                                                </div>
+                                                <div className="list-group m-2 flex flex-col gap-3">
+                                                    <Link to="/my-profile" className="text-lg">
+                                                        <FontAwesomeIcon className="mr-3" icon={faUser} /> Profile Page
+                                                    </Link>
+                                                    <Link to="/my-inventories" className="text-lg">
+                                                        <FontAwesomeIcon className="mr-3" icon={faFileInvoice} /> My Inventories
+                                                    </Link>
+                                                    <Link to="/" className="text-lg">
+                                                        <FontAwesomeIcon className="mr-3" icon={faSignOut} /> Sign Out
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </ul>
+                                )}
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-title hover:opacity-75" to="/login">
+                                    <FontAwesomeIcon className="mr-3" icon={faSignIn} /> Login
+                                </NavLink>
                             </li>
                         </ul>
                     </div>
