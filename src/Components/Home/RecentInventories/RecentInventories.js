fe6aa11a51c5052ee7112d4744e4cf02fcf5fa29
Shamim Reza <wpshamim@gmail.com>
2022-06-26 12:03:03 +0600

    add inventories on home page

--- src/Components/Home/RecentInventories/RecentInventories.js
+++ src/Components/Home/RecentInventories/RecentInventories.js
@@ -1,10 +1,29 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import useInventories from "../../../Hooks/useInventories";
+import "./RecentInventories.css";
+import RecentInventory from "./RecentInventory";
 
 const RecentInventories = () => {
+    const navigate = useNavigate();
+
+    const { inventories } = useInventories();
+
     return (
-        <div>
-            <h1>Recent Inventories</h1>
-        </div>
+        <section className="container mx-auto py-20 shadow-lg">
+            <div className="section-top text-center pb-20">
+                <h1 className="text-4xl font-bold text-title section-title">Recent Inventories</h1>
+            </div>
+
+            <div className="inventories grid grid-cols-3 gap-10 mb-14">
+                {inventories.map((inventory) => (
+                    <RecentInventory key={inventory._id} inventory={inventory} />
+                ))}
+            </div>
+
+            <button onClick={() => navigate("/manage-inventories")} className="bg-primary text-2xl px-8 py-2 font-bold text-white rounded mx-auto block">
+                See All Inventories
+            </button>
+        </section>
     );
 };
 
---
+++ src/Components/Home/RecentInventories/RecentInventory.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const RecentInventory = ({ inventory }) => {
+    const navigate = useNavigate();
+
+    const { name, price, stock, supplier, description, picture, _id } = inventory;
+    return (
+        <div className="box-wrapper rounded-lg shadow-2xl relative">
+            <img src={picture} alt={name} />
+            <div className="box-content ">
+                <h2 className="text-title font-bold text-3xl mb-5 pl-10">{name}</h2>
+                <div className="border-b mb-2 pb-2">
+                    <div className="pl-10 flex items-center justify-between  pr-5 ">
+                        <p className="text-lg">
+                            Stocks: <span className="text-primary font-bold">{stock}</span>
+                        </p>
+                        <p className="supplier text-lg ">
+                            Supplier: <span className="text-primary font-bold">{supplier}</span>
+                        </p>
+                    </div>
+                </div>
+                <div className="pl-10">
+                    <p className="text-paragraph pb-2 pr-5">{description}</p>
+                    <h4 className="text-title text-3xl font-bold pt-4 pb-8">${price}</h4>
+                </div>
+            </div>
+            <button onClick={() => navigate(`/inventory/${_id}`)} className="w-full bg-secondary text-white text-lg px-5 py-3 shadow">
+                Stock Update
+            </button>
+            <div class="ribbon ribbon-top-right">
+                <span className={`text-gray-700 ${stock > 10 ? "bg-green-400" : stock <= 10 && stock > 0 ? "bg-yellow-400" : "bg-red-400"}`}>{stock > 10 ? "Available" : stock <= 10 && stock > 0 ? "Low Stock" : "Sold"}</span>
+            </div>
+        </div>
+    );
+};
+
+export default RecentInventory;
